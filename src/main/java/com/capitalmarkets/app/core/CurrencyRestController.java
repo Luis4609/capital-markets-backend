92a8c6f567c4039a7b32d771e7ed85cdb1300a07
Luis Monzon <luis.monzon.pedreira@alumnos.upm.es>
2022-08-01 08:32:48 +0200

    Delete archetype, core, data, properties

---
+++ src/main/java/com/capitalmarkets/app/core/CurrencyRestController.java
@@ -0,0 +1,11 @@
+package com.capitalmarkets.app.core;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/currencies")
+public class CurrencyRestController {
+
+
+}
