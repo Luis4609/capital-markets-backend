68b6d47ee94de177aa7b9e88cd2f6b38b3228796
Andrés <andres.ramos@optimissa.com>
2022-08-02 10:13:07 +0200

    New method User Provider

--- src/main/java/com/capitalmarkets/app/data/providers/IUserProvider.java
+++ src/main/java/com/capitalmarkets/app/data/providers/IUserProvider.java
@@ -1,4 +1,10 @@
 package com.capitalmarkets.app.data.providers;
 
+
+import com.capitalmarkets.app.dto.UserDTO;
+
 public interface IUserProvider {
+    UserDTO getUserByName(String name);
+    UserDTO getUserByMail(String mail);
+    UserDTO getUserByPassword(String password);
 }
