91afc09441a26bc0265cee80d45466934bb4304c
OPTIMISSA\henoch.bueno <henoch.bueno@optimissa.com>
2022-08-02 13:35:07 +0200

    Include user exceptions

--- src/main/java/com/capitalmarkets/app/dto/UserDTO.java
+++ src/main/java/com/capitalmarkets/app/dto/UserDTO.java
@@ -5,11 +5,15 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class UserDTO {
+public class UserDTO extends ExceptionDTO{
 
     public String mail;
     public String name;
     public String dni;
     public String password;
+
+    public UserDTO(String exception){
+        super(exception);
+    }
 
 }
