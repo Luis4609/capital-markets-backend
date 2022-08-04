be586b581c6785fca14fc4d00da73e10f47b1925
OPTIMISSA\cristian.garcia <cristian.garcia@optimissa.com>
2022-08-04 12:40:23 +0200

    ﻿Squash

---
+++ src/main/java/com/capitalmarkets/app/core/dto/CurrencyDto.java
@@ -0,0 +1,15 @@
+package com.capitalmarkets.app.core.dto;
+
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class CurrencyDto {
+
+    private String code;
+
+    private String name;
+
+}
--- src/test/java/com/capitalmarkets/app/AppApplicationTests.java
+++ src/test/java/com/capitalmarkets/app/AppApplicationTests.java
@@ -1,5 +1,9 @@
 package com.capitalmarkets.app;
 
+import com.capitalmarkets.app.core.controller.CurrencyRestController;
+import com.capitalmarkets.app.data.daos.ICurrencyDao;
+import com.capitalmarkets.app.data.entities.CurrencyModel;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
