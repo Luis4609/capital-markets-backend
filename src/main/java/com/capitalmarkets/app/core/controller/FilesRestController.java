9855a456c9525371b909a7444ae338eb157ce01f
OPTIMISSA\henoch.bueno <henoch.bueno@optimissa.com>
2022-08-19 10:07:11 +0200

    Improves pdf and changes get methods to post methods

--- src/main/java/com/capitalmarkets/app/core/controller/FilesRestController.java
+++ src/main/java/com/capitalmarkets/app/core/controller/FilesRestController.java
@@ -18,16 +18,15 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -38,114 +37,144 @@ public class FilesRestController {
     @Autowired
     private IcurrencyControllerService controllerService;
 
-    @Autowired
-    private ResourceLoader resourceLoader;
 
     @GetMapping("/txtList-currencies")
-    public String getCurrenciesTxtList() throws IOException {
+    public HashMap<String, Object> getCurrenciesTxtList() throws IOException {
 
         String filePath = "capitalmarkets\\src\\main\\resources\\files\\currencies.txt";
-        FileOutputStream f = new FileOutputStream(filePath, true);
-        String lineToAppend = (controllerService.getAll()).toString();
-        byte[] byteArr = lineToAppend.getBytes();
-        f.write(byteArr);
-        f.close();
+        HashMap<String, Object> map = new HashMap<>();
 
-        return "Listado creado en txt";
+        try {
+            FileOutputStream f = new FileOutputStream(filePath, true);
+            String lineToAppend = (controllerService.getAll()).toString();
+            byte[] byteArr = lineToAppend.getBytes();
+            f.write(byteArr);
+            f.close();
+            map.put("message", "The txt was created");
+            map.put("path", filePath);
+            map.put("status", HttpStatus.OK);
+
+        }catch (Exception e) {
+            map.put("Message", "Internal error");
+            map.put("status", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return map;
     }
 
     @GetMapping("/excelList-currencies")
-    public String getCurrenciesExcelList() throws IOException {
+    public HashMap<String, Object> getCurrenciesExcelList() throws IOException {
 
-        String nombreArchivo = "CurrencieList.xlsx";
-        String rutaArchivo = "capitalmarkets\\src\\main\\resources\\files\\" + nombreArchivo;
-        String hoja = "Currencies";
+        String name = "CurrencieList.xlsx";
+        String path = "capitalmarkets\\src\\main\\resources\\files\\" + name;
+        String sheet = "Currencies";
 
-        XSSFWorkbook libro = new XSSFWorkbook();
-        XSSFSheet hoja1 = libro.createSheet(hoja);
+        HashMap<String, Object> map = new HashMap<>();
+        try {
+            XSSFWorkbook book = new XSSFWorkbook();
+            XSSFSheet sheet1 = book.createSheet(sheet);
 
-        String[] header = new String[]{"Código", "Nombre"};
-
-
-        List<CurrencyApiDTO> currencies = controllerService.getAll();
-        String[][] document2 = new String[currencies.size()][2];
+            String[] header = new String[]{"Código", "Nombre"};
 
 
-        for (int i = 0; i < currencies.size(); i++) {
-            document2[i][0] = currencies.get(i).getCode();
-            document2[i][1] = currencies.get(i).getName();
-        }
+            List<CurrencyApiDTO> currencies = controllerService.getAll();
+            String[][] document = new String[currencies.size()][2];
 
 
-        CellStyle style = libro.createCellStyle();
-        Font font = libro.createFont();
-        font.setBold(true);
-        style.setFont(font);
+            for (int i = 0; i < currencies.size(); i++) {
+                document[i][0] = currencies.get(i).getCode();
+                document[i][1] = currencies.get(i).getName();
+            }
 
 
-        //generar los datos para el documento
-        for (int i = 0; i <= currencies.size(); i++) {
-            XSSFRow row = hoja1.createRow(i);//se crea las filas
-            for (int j = 0; j < header.length; j++) {
-                //para la cabecera
-                if (i == 0) {
-                    //se crea las celdas para la cabecera, junto con la posición
-                    XSSFCell cell = row.createCell(j);
-                    // se añade el style crea anteriormente
-                    cell.setCellStyle(style);
-                    //se añade el contenido
-                    cell.setCellValue(header[j]);
-                    //para el contenido
-                } else {
-                    //se crea las celdas para la contenido, junto con la posición
-                    XSSFCell cell = row.createCell(j);
-                    //se añade el contenido
-                    cell.setCellValue(document2[i - 1][j]);
+            CellStyle style = book.createCellStyle();
+            Font font = book.createFont();
+            font.setBold(true);
+            style.setFont(font);
+
+
+            for (int i = 0; i <= currencies.size(); i++) {
+                XSSFRow row = sheet1.createRow(i);
+                for (int j = 0; j < header.length; j++) {
+
+                    if (i == 0) {
+
+                        XSSFCell cell = row.createCell(j);
+
+                        cell.setCellStyle(style);
+
+                        cell.setCellValue(header[j]);
+
+                    } else {
+
+                        XSSFCell cell = row.createCell(j);
+
+                        cell.setCellValue(document[i - 1][j]);
+                    }
                 }
             }
-        }
 
-        File file;
-        file = new File(rutaArchivo);
-        try (FileOutputStream fileOuS = new FileOutputStream(file)) {
-            if (file.exists()) {// si el archivo existe se elimina
-                file.delete();
-                System.out.println("Archivo eliminado");
+            File file;
+            file = new File(path);
+            try (FileOutputStream fileOuS = new FileOutputStream(file)) {
+                if (file.exists()) {
+                    file.delete();
+                    System.out.println("Archivo eliminado");
+                }
+                book.write(fileOuS);
+                fileOuS.flush();
+                fileOuS.close();
+                System.out.println("Archivo Creado");
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            libro.write(fileOuS);
-            fileOuS.flush();
-            fileOuS.close();
-            System.out.println("Archivo Creado");
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            map.put("message", "The excel was created");
+            map.put("path", path);
+            map.put("status", HttpStatus.OK);
+
+        } catch (Exception e) {
+            map.put("Message", "Internal error");
+            map.put("status", HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-        return "Listado creado en excel";
+        return map;
     }
 
 
-    @GetMapping("/txtHistorical")
-    public String getHistoricalTxt(String date, double value, String base, String conversion) throws IOException {
+    @PostMapping("/txtHistorical")
+    public HashMap<String, Object> getHistoricalTxt(@RequestBody CurrencyHistoricalDTO dto) throws IOException {
 
         String filePath = "capitalmarkets\\src\\main\\resources\\files\\historical.txt";
-        FileOutputStream f = new FileOutputStream(filePath, true);
-        String lineToAppend = (controllerService.getHistorical(date, value, base, conversion)).toString();
-        byte[] byteArr = lineToAppend.getBytes();
-        f.write(byteArr);
-        f.close();
 
-        return "Historico creado en txt";
+
+        HashMap<String, Object> map = new HashMap<>();
+        try {
+            FileOutputStream f = new FileOutputStream(filePath, true);
+            String lineToAppend = (controllerService.getHistorical(dto.getStartDate(), dto.getAmount(), dto.getBase(), dto.getConversion())).toString();
+            byte[] byteArr = lineToAppend.getBytes();
+            f.write(byteArr);
+            f.close();
+            map.put("message", "The txt was created");
+            map.put("path", filePath);
+            map.put("status", HttpStatus.OK);
+
+
+        } catch (Exception e) {
+            map.put("Message", "Internal error");
+            map.put("status", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return map;
     }
 
-    @SneakyThrows
-    @GetMapping("/pdfHistorical")
-    public String getHistoricalPdf(String start, String end, double value, String base, String conversion) throws IOException {
 
+    @PostMapping("/pdfHistorical")
+    public HashMap<String, Object> getHistoricalPdf(@RequestBody CurrencyHistoricalDTO dto) throws IOException {
 
-        CurrencyHistoricalDTO texto = (controllerService.getInterval(start, end, value, base, conversion));
+        String path = "capitalmarkets\\src\\main\\resources\\files\\historical.pdf";
+        CurrencyHistoricalDTO text = (controllerService.getInterval(dto.getStartDate(), dto.getEndDate(), dto.getAmount(), dto.getBase(), dto.getConversion()));
 
         PDDocument document = new PDDocument();
         PDPage page = new PDPage(PDRectangle.A4);
@@ -154,14 +183,27 @@ public class FilesRestController {
 
 
         headerPdf(contentStream, document, page);
-        infoPdf(contentStream, texto, page);
-        dataPdf(contentStream, document, page, texto);
+        infoPdf(contentStream, text, page);
+        dataPdf(contentStream, document, page, text);
 
 
-        document.save("capitalmarkets\\src\\main\\resources\\files\\historical.pdf");
+        HashMap<String, Object> map = new HashMap<>();
+        try {
 
+            document.save(path);
 
-        return "Historico creado en pdf";
+            map.put("message", "The pdf was created");
+            map.put("path", path);
+            map.put("status", HttpStatus.OK);
+
+        } catch (Exception e) {
+
+            map.put("Message", "Internal error");
+            map.put("status", HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+        return map;
+
     }
 
 
@@ -187,23 +229,23 @@ public class FilesRestController {
         contentStream.beginText();
         contentStream.setFont(PDType1Font.COURIER_BOLD_OBLIQUE, 12);
         contentStream.newLineAtOffset(20, page.getMediaBox().getHeight() - 100);
-        contentStream.showText("FECHA: ");
+        contentStream.showText("    FECHA: ");
         contentStream.newLine();
         contentStream.setFont(PDType1Font.COURIER, 9);
-        contentStream.showText("Desde: " + texto.getStartDate() + " Hasta: " + texto.getEndDate());
+        contentStream.showText("     Desde: " + texto.getStartDate() + " Hasta: " + texto.getEndDate());
         contentStream.newLine();
         contentStream.setFont(PDType1Font.COURIER_BOLD_OBLIQUE, 12);
-        contentStream.showText("DE: ");
+        contentStream.showText("    DE: ");
         contentStream.setFont(PDType1Font.COURIER, 9);
         contentStream.showText(texto.getBase());
         contentStream.newLine();
         contentStream.setFont(PDType1Font.COURIER_BOLD_OBLIQUE, 12);
-        contentStream.showText("A: ");
+        contentStream.showText("    A: ");
         contentStream.setFont(PDType1Font.COURIER, 9);
         contentStream.showText(texto.getTo());
         contentStream.newLine();
         contentStream.setFont(PDType1Font.COURIER_BOLD_OBLIQUE, 12);
-        contentStream.showText("VALOR ACTUAL: ");
+        contentStream.showText("    VALOR ACTUAL: ");
         contentStream.setFont(PDType1Font.COURIER, 9);
         contentStream.showText(texto.getConversion());
         contentStream.newLine();
@@ -221,7 +263,7 @@ public class FilesRestController {
         contentStream.beginText();
         contentStream.setFont(PDType1Font.COURIER_BOLD_OBLIQUE, 12);
         contentStream.newLineAtOffset(20, page.getMediaBox().getHeight() - 180);
-        contentStream.showText("HISTORICO DE VALORES: ");
+        contentStream.showText("    HISTORICO DE VALORES: ");
         contentStream.newLine();
         contentStream.newLine();
 
@@ -235,13 +277,14 @@ public class FilesRestController {
 
             for (Field field : texto.getRates().get(i).getClass().getDeclaredFields()) {
                 field.setAccessible(true);
-                builder.append(field.getName())
+                builder.append("      ")
+                        .append(field.getName())
                         .append(" = ")
                         .append(field.get(o))
                         .append("                                              ");
             }
 
-            System.out.println("builder: " + builder.toString());
+            System.out.println("builder: " + builder);
 
             line++;
 
