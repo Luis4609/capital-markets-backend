ddb394bd474e26a82e4d37b812f24e52bd448f37
jesus.casanova <jesus.casanova@otimissa.com>
2022-08-22 10:00:19 +0200

    [#27495] Finish usercontroller tests.

--- src/test/java/com/capitalmarkets/app/core/service/UserControllerServiceTestIntegration.java
+++ src/test/java/com/capitalmarkets/app/core/service/UserControllerServiceTestIntegration.java
@@ -2,7 +2,8 @@ package com.capitalmarkets.app.core.service;
 
 
 import com.capitalmarkets.app.core.controller.UserRestController;
-import com.capitalmarkets.app.core.services.UserControllerService;
+import static org.assertj.core.api.Assertions.assertThat;
+import com.capitalmarkets.app.dto.data.UserDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,9 +16,47 @@ public class UserControllerServiceTestIntegration {
 
 
     @Test
-    public void registerTest(){
+    public void registerTestOK(){
+        UserDTO userdto = new UserDTO("Luis", "Monzón", "50540431Y", "luis@gmail.com", "1234");
 
+            assertThat(userdto).isNotNull();
+            assertThat(userdto.getName()).isNotEmpty();
+            assertThat(userdto.getSurname()).isNotEmpty();
+            assertThat(userdto.getDni()).isNotEmpty();
+            assertThat(userdto.getMail()).isNotEmpty();
+            assertThat(userdto.getPassword()).isNotEmpty();
+            assertThat(userdto.getName()).isNotNull();
+            assertThat(userdto.getSurname()).isNotNull();
+            assertThat(userdto.getDni()).isNotNull();
+            assertThat(userdto.getMail()).isNotNull();
+            assertThat(userdto.getPassword()).isNotNull();
+            assertThat(userdto.getDni()).hasSize(9);
+            assertThat(userdto.getName()).isEqualToNormalizingPunctuationAndWhitespace("Luis");
+            assertThat(userdto.getSurname()).isEqualToNormalizingPunctuationAndWhitespace("Monzón");
+            assertThat(userdto.getDni()).endsWith("Y");
+            //assertThat(userdto.getMail()).hasSizeBetween(4, 8);
 
     }
 
+    @Test
+    public void findByMailTestOK(){
+        UserDTO userDTO = new UserDTO("Luis", "Monzón", "50540431Y", "luis@gmail.com", "1234");
+
+            assertThat(userDTO).isNotNull();
+            assertThat(userDTO.getMail()).isNotNull();
+            assertThat(userDTO.getMail()).isNotEmpty();
+            assertThat(userDTO.getMail()).isEqualToIgnoringCase("luis@gmail.com");
+            assertThat(userDTO.getMail()).isEqualToNormalizingPunctuationAndWhitespace("luis@gmail.com");
+    }
+
+    @Test
+    public void verifyPasswordTest(){
+        UserDTO userDTO = new UserDTO("Luis", "Monzón", "50540431Y", "luis@gmail.com", "1234");
+
+            assertThat(userDTO).isNotNull();
+            assertThat(userDTO.getPassword()).isNotEmpty();
+            assertThat(userDTO.getPassword()).isNotNull();
+            assertThat(userDTO.getPassword()).isEqualToNormalizingPunctuationAndWhitespace("1234");
+            assertThat(userDTO.getPassword()).hasSizeBetween(4, 8);
+    }
 }
